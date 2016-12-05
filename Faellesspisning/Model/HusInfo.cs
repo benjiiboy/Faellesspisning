97b6f0d200f4cbb0b6cfee7e45fd3b93c59e1b02
benjamin lindgaard <benjaminlindgaard@ka-net.dk>
2016-12-05 13:14:06 +0100

    lavede metode og props

--- Faellesspisning/Model/HusInfo.cs
+++ Faellesspisning/Model/HusInfo.cs
@@ -9,18 +9,58 @@ namespace Faellesspisning
    public class HusInfo
     {
 
-        //instance field
-        public double TotalDagsPris;
-        public double TotalAntalDeltagere;
-        public double PrisPerPerson;
-        public double PrisPerHusstand;
-        public double AntalPersonerIHusstand;
-        public int AntalVoksneIHusstand;
-        public int AntalTeenagerIHusstand;
-        public int AntalBarnIHusstand;
-        public int AntalBabyIHusstand;
+        
 
-        public string HusNummer;
+
+
+
+
+        //instance field
+        public double Mandag;
+        public double Tirsdag;
+        public double Onsdag;
+        public double Torsdag;
+        public double Fredag;
+        public double Lørdag;
+        public double Søndag;
+        
+
+        //PROPS
+        public double TotalDagsPris
+        {
+            get { return TotalUgePris(); }
+            set
+            {
+                ;
+            }
+        }
+
+
+
+        public double TotalAntalDeltagere { get; set; }
+        public double PrisPerPerson { get; set; }
+        public double PrisPerHusstand { get; set; }
+
+        public double AntalPersonerIHusstand { get; set; }
+
+        public int AntalVoksneIHusstand { get; set; }
+        public int AntalTeenagerIHusstand { get; set; }
+        public int AntalBarnIHusstand { get; set; }
+        public int AntalBabyIHusstand { get; set; }
+
+        public string HusNummer { get; set; }
+
+
+        //Metode
+
+        public double TotalUgePris()
+        {
+            return Mandag + Tirsdag + Onsdag + Torsdag + Fredag + Lørdag + Søndag;
+        }
+
+
+
+
 
     }
 }

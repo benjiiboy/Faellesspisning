235468bf0e540f67671a50a9e3ab82904f5085ec
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2016-12-05 12:46:24 +0100

    lavet instance field

--- Faellesspisning/Model/HusInfo.cs
+++ Faellesspisning/Model/HusInfo.cs
@@ -8,7 +8,19 @@ namespace Faellesspisning
 {
    public class HusInfo
     {
-  
+
+        //instance field
+        public double TotalDagsPris;
+        public double TotalAntalDeltagere;
+        public double PrisPerPerson;
+        public double PrisPerHusstand;
+        public double AntalPersonerIHusstand;
+        public int AntalVoksneIHusstand;
+        public int AntalTeenagerIHusstand;
+        public int AntalBarnIHusstand;
+        public int AntalBabyIHusstand;
+
+        public string HusNummer;
 
     }
 }

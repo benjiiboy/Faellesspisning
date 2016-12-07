aeb1891f1cc1c6487e75e585c4f8cea1f0e9eb0c
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2016-12-07 09:54:17 +0100

    Tilføjet BudgetViewmodel

---
+++ Faellesspisning/Viewmodel/BudgetViewmodel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faellesspisning.Viewmodel
+{
+    class BudgetViewmodel : INotifyPropertyChanged
+    {
+        
+        public int GetNummer9()
+        {
+            return 9;
+        }
+
+        #region vores PropertyChangedEventHandler 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
